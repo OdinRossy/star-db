ea3b7373e22c43cd3c8e0cb93d412e62cba81560
OdinRossy <remniov.work@gmail.com>
2019-11-18 22:13:56 +0300

    Updates

---
+++ precache-manifest.8f695265827aefa806ec89095493807e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae588bdb8f87419d2e24a6170c920d1",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "8d13ebb0d1fa77211b57",
+    "url": "/star-db/static/css/main.955087e7.chunk.css"
+  },
+  {
+    "revision": "7e3a8d46dda733019970",
+    "url": "/star-db/static/js/2.bcd7d204.chunk.js"
+  },
+  {
+    "revision": "8d13ebb0d1fa77211b57",
+    "url": "/star-db/static/js/main.22a24315.chunk.js"
+  },
+  {
+    "revision": "0116862b95b85cdbd8c4",
+    "url": "/star-db/static/js/runtime-main.b6f34180.js"
+  }
+]);
