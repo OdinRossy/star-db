50dcba97442eb718724b721d725988cdfa4e6862
OdinRossy <remniov.work@gmail.com>
2020-02-14 12:52:30 +0300

    FEAT: add toggleRandomPlanet button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,11 +7,32 @@ import ItemList from "../ItemList";
 import PersonDetails from "../PersonDetails";
 
 export default class App extends Component {
+
+    state = {
+        showRandomPlanet: true
+    };
+
+    toggleShowRandomPlanet = () => {
+        this.setState(({showRandomPlanet}) => {
+            return {
+                showRandomPlanet: !showRandomPlanet
+            }
+        })
+    };
+
     render() {
         return (
             <div>
                 <Header/>
-                <RandomPlanet/>
+                {
+                    this.state.showRandomPlanet ? <RandomPlanet/> : null
+                }
+
+                <button
+                    className="toggle-planet btn btn-warning btn-lg"
+                    onClick={this.toggleShowRandomPlanet}>
+                    Toggle Random Planet
+                </button>
 
                 <div className="row mb2">
                     <div className="col-md-6 mb2">
