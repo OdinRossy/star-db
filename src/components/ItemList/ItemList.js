9ed9f38db3d4c6a90ac6bdfad130095e1ea9c24e
OdinRossy <remniov.work@gmail.com>
2020-03-07 14:31:01 +0300

    73. Render functions

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -37,29 +37,23 @@ export default class ItemList extends Component {
         this.setState({hasErrors: true, isLoaded: true})
     };
 
+    renderItems = (items, onItemSelected) => {
+        return items.map(item => (
+                <li key={item.id}
+                    className="list-group-item"
+                    onClick={() => onItemSelected(item.id)}
+                >{this.props.renderItem(item)}
+                </li>
+            )
+        )
+    };
+
     render() {
         const {itemList, isLoaded, hasErrors} = this.state;
 
-        return isLoaded ?
-            hasErrors ? <ErrorIndicator/> :
-                <ItemListView
-                    itemList={itemList}
-                    onItemSelected={this.props.onItemSelected}/> : <Spinner/>
+        return isLoaded ? hasErrors ? <ErrorIndicator/> :
+            <ul className="item-list list-group">
+                {this.renderItems(itemList, this.props.onItemSelected)}
+            </ul> : <Spinner/>
     }
 }
-
-const ItemListView = ({itemList, onItemSelected}) => {
-    const itemElements = itemList.map(item => (
-        <li key={item.id}
-            className="list-group-item"
-            onClick={() => onItemSelected(item.id)}
-        >{item.name}
-        </li>
-    ));
-
-    return (
-        <ul className="item-list list-group">
-            {itemElements}
-        </ul>
-    );
-};
--- src/components/PersonPage/PersonPage.js
+++ src/components/PersonPage/PersonPage.js
@@ -38,6 +38,7 @@ export default class PersonPage extends Component {
                         <ItemList
                             onItemSelected={this.onPersonSelected}
                             getData={this.swapiService.getAllPeople}
+                            renderItem={({name, gender, birthYear}) => `${name} (${gender}, ${birthYear})`}
                         />
                     </div>
                     <div className="col-md-6">
